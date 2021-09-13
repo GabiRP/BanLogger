d9f68fe082ceca954e70d8918aefe4842e099717
GabiRP <ruizperezgabriel@hotmail.com>
2021-09-13 20:39:19 +0200

    Added UserIP for private embeds

--- Features/Structs/UserInfo.cs
+++ Features/Structs/UserInfo.cs
@@ -1,17 +1,21 @@
-﻿namespace BanLogger.Features.Structs
+﻿using System.Text;
+
+namespace BanLogger.Features.Structs
 {
     public struct UserInfo
     {
-        public UserInfo(string username, string userId)
+        public UserInfo(string username, string userId, string userIP)
         {
             Username = username;
             UserId = userId;
+            UserIP = userIP;
         }
         
         public string Username;
         public string UserId;
+        public string UserIP;
         
         public string PublicInfo => string.IsNullOrEmpty(Username) ? "n/a" : Username;
-        public string PrivateInfo => $"{(string.IsNullOrEmpty(Username) ? "n/a" : Username)} ({(string.IsNullOrEmpty(UserId) ? "n/a" : UserId)})";
+        public string PrivateInfo => $"{(string.IsNullOrEmpty(Username) ? "n/a" : Username)} ({(string.IsNullOrEmpty(UserId) ? "n/a" : UserId)}) ||{(string.IsNullOrEmpty(UserIP) ? "n/a" : UserIP)}||";
     }
 }
